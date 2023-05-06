daf749bffc59e3cbf51190f5df814f0bb818d731
Shiv-Raj-Singh <mt932747@gmail.com>
2023-05-06 17:47:25 +0530

    first commit for check contact project

--- my-portfolio/src/Components/Projects/data.js
+++ my-portfolio/src/Components/Projects/data.js
@@ -1,16 +1,96 @@
 const projectData = [
   {
+    name: "Slot Booking App",
+    description: "MERN Stack",
+    body: [
+      'In this Project Performed Slot Booking Operation',
+      'Anyone can Book the slot in future time and Slot must be not booked already',
+      'Date and Time Should be Valid and Upcoming',
+      'Dependencies React.Js ,NodeJs ,ExpressJs ,MySQL so on'
+    ],
+    github : 'https://github.com/Shiv-Raj-Singh/Slot-Book-Task.git',
+    live : true,
+    link : 'https://slot-books.netlify.app/'
+  },
+  {
+    name: "InternShala Clone",
+    description: "MERN Stack",
+    body: [
+      'In this Project build a Clone of InternShala Application',
+      'Student Register and capitalize the available opportunities ',
+      'Colleges and Organizations can offer the opportunities',
+      'Dependencies React.Js ,NodeJs ,ExpressJs ,MongoDB , JWT , Mongoose and so on'
+    ],
+    github : 'https://github.com/Shiv-Raj-Singh/Projects/tree/Inter-Shala-Clone',
+    live : false,
+    link : ''
+  },
+  {
+    name: "Blog Management App",
+    description: "MERN Stack",
+    body: [
+      'In this Project Performed REST APIs for User and Blogs',
+      "Where user has to Register account And LogIn",
+      'User can create Blogs and perform CRUD operations',
+      "while User wanna perform CRUD operation user must be authorized ",
+      'Dependencies React.Js ,NodeJs ,ExpressJs ,MongoDB , Joi and so on'
+    ],
+    github : 'https://github.com/Shiv-Raj-Singh/Projects/tree/blogging-site-project-1',
+    live : false,
+    link : 'https://slot-books.netlify.app/'
+  },
+  {
+    name: "URL - Shortner",
+    description: "MERN Stack",
+    body: [
+      "in this Project We can put long URL and in Return get a new Short URL",
+      'that new Short Url will be work on any browser and re-direct on main url result',
+      'Url Should be Valid and working',
+      'Dependencies React.Js ,NodeJs ,ExpressJs ,MongoDB, Redis axios etc '
+    ],
+    github : 'https://github.com/Shiv-Raj-Singh/Url---Shortner---Project--4',
+    live : true,
+    link : 'https://url-shortner-project-4.vercel.app/'
+  },
+  {
+    name: "To-Do App",
+    description: "React.Js Bootstrap",
+    body: [
+      "in this project we have performed To-do tasks operations",
+      "Where user has to Register account",
+        'User can create any task and perform CRUD operations',
+      'Dependencies React , CSS , Local-Storage and Bootstrap for User-Interface'
+    ],
+    github : 'https://github.com/Shiv-Raj-Singh/To-Do-Task/tree/main/build_formula_task',
+    live : true,
+    link : 'https://url-shortner-project-4-85wl.vercel.app/'
+  },
+  {
+    name: "Chatting App",
+    description: "MERN Stack",
+    body: [
+      "in this project we have done REST APIs for Users",
+      "Where user has to Register account with strong validation (unique email & phone strong password )",
+      "he has to login in return he will get a JWT Token and Select a desire Room to start Chat",
+      "if User forget the Password then he can Reset that easily by email",
+      'Dependencies NodeJs , express.Js, Socket.IO , MongoDB , Mongoose , JWT (generate auth token ) , bcrypt (Hash the Password) , Joi (Validate the data ) , Nodemailer(sent-email),so on' ,
+      'Used React , CSS and Bootstrap for User-Interface'
+    ],
+    github : 'https://github.com/Shiv-Raj-Singh/Amazon-Clone-App.git',
+    live : true,
+    link : 'https://chat-app-frontend-sigma.vercel.app/'
+  },
+  {
     name: "Amazon-Clone App",
     description: "NodeJs ExpressJs MongoDB",
     body: [
       "in this project we have done REST APIs for User,Product,Cart and Order",
       "Where user has to Register account with strong validation (unique email & phone strong password )",
       "he has to login in return he will get a JWT Token",
-      "Implanted Authentication and Authorization for create new Books , update existing books , delete existing books",
-      "if any User or Guest want to visit the app and read the books then he can only those books he can read which are published and as well as he can give the review on them ",
-      'in this project i used NodeJs , express Js, MongoDB , Mongoose , JWT (generate auth token ) , bcrypt (Hash the Password) , Joi (Validate the data ) ,'
+      "Performed Authentication and Authorization for add product in cart and order them",
+      'Dependencies NodeJs , express.Js, MongoDB , Mongoose , JWT (generate auth token ) , bcrypt (Hash the Password) , Joi (Validate the data ) , Nodemailer(sent-email) so on'
     ],
-    github : 'https://github.com/Shiv-Raj-Singh/Projects/tree/project/book-management',
+    github : 'https://github.com/Shiv-Raj-Singh/Amazon-Clone-App.git',
     live : false,
     link : ''
   },
@@ -21,7 +101,7 @@ const projectData = [
       "in this project we have done REST APIs for User , Books and Reviews",
       "Where user has to Register account with strong validation (unique email & phone strong password )",
       "he has to login in return he will get a JWT Token",
-      "Implanted Authentication and Authorization for create new Books , update existing books , delete existing books",
+      "Performed Authentication and Authorization for create new Books , update existing books , delete existing books",
       "if any User or Guest want to visit the app and read the books then he can only those books he can read which are published and as well as he can give the review on them ",
       'in this project i used NodeJs , express Js, MongoDB , Mongoose , JWT (generate auth token ) , bcrypt (Hash the Password) , Joi (Validate the data ) ,'
     ],
