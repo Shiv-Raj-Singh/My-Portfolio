4cf38fe89201fe3123d9919a91b22842d1c5f4e4
Shiv-Raj-Singh <mt932747@gmail.com>
2023-05-08 18:42:44 +0530

    done

--- my-portfolio/src/app.js
+++ my-portfolio/src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import sendMailFunction  from './NodeMailer.js';
+import cors from 'cors';
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 
 app.get('/' , (req,res)=>{
     res.send("Hi Welcome on Server of portfolio ")
